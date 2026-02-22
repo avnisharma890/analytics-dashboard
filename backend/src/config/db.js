8c943ffa889ee98a4f3c392e823d4646b4e980d9
avni sharma <avnis7733@gmail.com>
2026-02-22 23:41:02 +0530

    ssl issue resolved

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,9 +3,13 @@ require('dotenv').config();
 
 console.log("DB URL:", process.env.DATABASE_URL);
 
+const isLocal =
+  process.env.DATABASE_URL?.includes('localhost') ||
+  process.env.DATABASE_URL?.includes('127.0.0.1');
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ssl: isLocal ? false : { rejectUnauthorized: false },
 });
 
 module.exports = pool;
